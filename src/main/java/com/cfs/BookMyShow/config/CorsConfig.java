24fe8c60d52c2b91aba44b3b74a39a94e5d9b738
ansul-18 <ansulverma034@gmail.com>
2026-04-01 16:41:40 +0530

    fianl

--- src/main/java/com/cfs/BookMyShow/config/CorsConfig.java
+++ src/main/java/com/cfs/BookMyShow/config/CorsConfig.java
@@ -30,9 +30,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")   // 👈 change THIS
-                .allowedOrigins("http://127.0.0.1:5500")  // 👈 avoid "*"
-                .allowedMethods("*")
+        registry.addMapping("/api/**")   // 👈 change THIS
+                .allowedOrigins("*")  // 👈 avoid "*"
+                .allowedMethods("GET","POST","PUT", "DELETE")
                 .allowedHeaders("*");
     }
 }
